502861ae3bdcacb194ec5a19ecfe34cc6320327a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-18 18:56:21 -0300

    Criação e utilização do componente CardFilm

--- app/Components/Header/header.tsx
+++ app/Components/Header/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <img src="https://i.pinimg.com/originals/dd/53/b6/dd53b6ee7cc7fea090ecc9666f20b7af.jpg" alt="Logo" className="logo" />
+        <img src="https://th.bing.com/th/id/R.7534a39f4ee178794a31ba9e28e279a8?rik=6cTCxSb3opPaAg&pid=ImgRaw&r=0" alt="Logo" className="logo" />
         <h1 className="title">Os melhores filmes estão aqui</h1>
       </div>
     </header>
